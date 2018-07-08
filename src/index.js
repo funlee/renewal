e414be0dfcb0969ee64b2302167511a7beda1998
funlee <i@funlee.cn>
2018-07-08 17:27:49 +0800

    redux init

--- src/index.js
+++ src/index.js
@@ -4,13 +4,52 @@
  * @Date: 2018-05-21 09:37:17
  * @Description: 项目入口 
  */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import createHistory from "history/createBrowserHistory";
+import { Route } from "react-router";
+
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from "react-router-redux";
+import reducers from "./reducers"; // Or wherever you keep your reducers
+
 import './assets/style/index.scss';
 import 'antd/dist/antd.css';
 import './assets/scripts/axiosConfig';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a history of your choosing (we're using a browser history in this case)
+const history = createHistory();
+
+// Build the middleware for intercepting and dispatching navigation actions
+const middleware = routerMiddleware(history);
+
+// Add the reducer to your store on the `router` key
+// Also apply our middleware for navigating
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    router: routerReducer
+  }),
+  applyMiddleware(middleware)
+);
+
+// Now you can dispatch navigation actions from anywhere!
+// store.dispatch(push('/foo'))
+
+ReactDOM.render(
+  <Provider store={store}>
+    {/* ConnectedRouter will use the store from Provider automatically */}
+    <ConnectedRouter history={history}>
+      <div>
+        <Nav />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
